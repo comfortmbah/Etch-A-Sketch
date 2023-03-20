0e7a2bc94a6aeab0b90d76d8e8ec2b62423c910f
Sylvester Mbah <sylvestermbah90@gmail.com>
2023-03-20 10:52:21 -0700

    Set up a “hover” effect that changes the grid divs background color

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const grid = document.getElementById('grid'); 
 
+function changeColor(e) {
+    const randomR = Math.floor(Math.random() * 256);
+    const randomG = Math.floor(Math.random() * 256);
+    const randomB = Math.floor(Math.random() * 256);
+    e.target.style.background = `rgb(${randomR}, ${randomG}, ${randomB})`;
+}
+
 function makeGrid(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     for (let i = 0; i < (size * size); i++) {
         const cell = document.createElement('div');
-        cell.classList.add('grid-item');
+        cell.addEventListener('mouseover', changeColor);
+        cell.addEventListener('mousedown', changeColor);
         grid.appendChild(cell);
     }
 }
